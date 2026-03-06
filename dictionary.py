d557d5eda0fd58d2c32cc5f2a7485d73080dfe00
BathTubTheGreat <yilyanel@nycstudents.net>
2026-03-06 10:17:09 -0500

    wesg

--- dictionary.py
+++ dictionary.py
@@ -1,18 +1,20 @@
 def itemlist():
-        6650 = {
+        itemlist = {
+            6650 = {
             "name": "AMD RX 6650 XT",
             "price": 249.99,
             "description": "CUTTING EDGE NEW GPU TECHNOLOGIES AND AMD BEST"
-        }
-        5090 = {
+            },
+            5090 = {
             "name": "Nvidia 5090",
             "price": 3699.99,
             "description": "best of best gpu with ai upscaling and frame gen"
-        }
-        sixtyfourgigram = {
-            "name": "64gb ram"
+            },
+            sixtyfourgigram = {
+            "name": "64gb ram",
             "price": 799.99,
             "description": "ram prices really gone up nowadays"
+            },
         }
         for index, item in enumerate(itemlist):
             print(index, ":", item[name])
